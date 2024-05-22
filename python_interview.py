cc1a2ea33b358a12d50b4e359c47178e12352c6d
alphahulk <65523906+alphahulk@users.noreply.github.com>
2024-05-23 02:10:54 +0530

    Update python_interview.py

--- python_interview.py
+++ python_interview.py
@@ -448,6 +448,27 @@ So, the number of trailing zeros in
 
 How to Remove Letters From a String in Python
 input_string = 'Geeks123For123Geeks'
+
+
+
+
+
+# Python3 code to demonstrate working of
+# Words Frequency in String Shorthands
+# Using dictionary comprehension + count() + split()
+ 
+# Initializing string
+test_str = 'Gfg is best . Geeks are good and Geeks like Gfg'
+ 
+# Printing original string
+print("The original string is : " + str(test_str))
+ 
+# Words Frequency in String Shorthands
+# Using dictionary comprehension + count() + split()
+res = {key: test_str.count(key) for key in test_str.split()}
+ 
+# Printing result
+print("The words frequency : " + str(res))
 output_string = ''.join([char for char in input_string if not char.isdigit()])
 
 print(output_string)
